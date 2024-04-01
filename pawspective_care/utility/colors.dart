f1c5dd57d00441570c63a14aee11bf0f8941488a
najwanzaky <najwanzaky21@gmail.com>
2024-04-01 13:16:44 +0700

    [Add] file for color pallete

---
+++ pawspective_care/utility/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xff1f2544);
+  
+  static const Color primary = Color(0xffffd0ec);
+  static const Color secondary = Color(0xff81689d);
+  static const Color third = Color(0xff474f7a);
+  
+}
