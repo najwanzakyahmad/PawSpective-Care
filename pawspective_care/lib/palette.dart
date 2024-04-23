873d1589cab809a0ba6da4dfad53c1deb00666d8
Aryagara Kristandy <72059961+kristandy@users.noreply.github.com>
2024-04-23 09:51:53 +0700

    [Add] palete.dart
    color palette

---
+++ pawspective_care/lib/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color mainColor = Color.fromRGBO(31, 37, 68, 1.0);
+  static const Color secondaryColor = Color.fromRGBO(71, 79, 122, 1.0);
+  static const Color thirdColor = Color.fromRGBO(129, 104, 157, 1.0);
+  static const Color fourthColor = Color.fromRGBO(255, 208, 236, 1.0);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1.0);
+}
